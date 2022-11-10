21c69b4f28c1ccf35b62e5e3b5f9e359aee9464d
Jeremy Gray <jgray@brainstation.io>
2022-11-10 00:13:27 -0500

    added scoring function

--- js/functions.js
+++ js/functions.js
@@ -5,3 +5,24 @@ function increment(param) {
 function decrement(param){
     document.getElementById(`${param}`).stepDown();
 }
+
+function roundUp(num){
+    return -1 * Math.floor(-num / 100) * 100;
+}
+
+function scoring(ron){
+    let dealer = document.getElementById('dealerCheckbox').checked;
+    let han = document.getElementById('hanCounter').value;
+    let fu = document.getElementById('fuCounter').value;
+    let honba = document.getElementById('honbaCounter').value;
+
+    switch(han){
+        default:
+            let basicPoints = fu * 2 ** (2 + han);
+            basicPoints = Math.min(2000,basicPoints);
+    }
+
+    if(!dealer && !ron){
+        return [roundUp(basicPoints * 2) + (honba * 100), roundUp(basicPoints) + (honba * 100)];
+    }
+}
